94e5780cc1655a9e9f83dff55a676a883814c5cc
amin <amin.nasim76@gmail.com>
2023-06-12 13:09:37 -0600

    russian-roulette and accurate sampling distance has been added. Writing ppm file had a small issue which is fixed.

--- render.py
+++ render.py
@@ -24,36 +24,46 @@ class Render:
             return bg
         
         num_samples = math.floor((isect['t1'] - isect['t0']) / self.step_size)
+        if num_samples > 0:
+            step_size = (isect['t1'] - isect['t0']) / num_samples
+        else:
+            return bg
+
         if self.backward_raymarching:
             result = bg
-            sample_transparency = math.exp(-self.step_size * (obj.scattering + obj.absorption))
+            sample_transparency = math.exp(-step_size * (obj.scattering + obj.absorption))
             for i in range(num_samples):
-                t = isect['t1'] - self.step_size * (i + random.random())
+                t = isect['t1'] - step_size * (i + random.random())
                 sample_pos = ray_orig + t * ray_dir    
                 light_isect = obj.intersect(sample_pos, -1 * self.light_dir)
-                light_attenuation = math.exp(- light_isect['t1'] * (obj.scattering + obj.absorption))
+                light_attenuation = math.exp(-light_isect['t1'] * (obj.scattering + obj.absorption))
                 result = result + self.light_color * light_attenuation *\
                         obj.phase(sample_pos, -light_dir, -ray_dir) * obj.scattering *\
-                        self.step_size
+                        step_size
                 result = result * sample_transparency
             return result
 
         else:
             transparency = 1.
             result = Vec3()
-            sample_transparency = math.exp(-self.step_size * (obj.scattering + obj.absorption))
+            d = 5 # Rusiian roulette chance
+            sample_transparency = math.exp(-step_size * (obj.scattering + obj.absorption))
             for i in range(num_samples):
-                t = isect['t0'] + self.step_size * (i + random.random())
+                t = isect['t0'] + step_size * (i + random.random())
                 sample_pos = ray_orig + t * ray_dir
                 transparency *= sample_transparency
                 light_isect = obj.intersect(sample_pos, -1 * self.light_dir)
                 light_attenuation = math.exp(- light_isect['t1'] * (obj.scattering + obj.absorption))
                 result = result + transparency * self.light_color * light_attenuation *\
                         obj.scattering * obj.phase(sample_pos, -light_dir, -ray_dir) *\
-                        self.step_size
-                # Stop is we have negligible transparency left
-                if transparency < 0.001:
-                    break
+                        step_size
+                # Russian roulette if we have negligible transparency left
+                if transparency < 0.01:
+                    if random.random() < (1/d):
+                        break
+                    else:
+                        transparency *= d
+
             return bg * transparency + result
 
     def render(self, camera, obj, bg):
@@ -62,6 +72,7 @@ class Render:
         focal = math.tan(math.radians(camera.fov * 0.5))
         result = []
         for j in range(camera.H):
+            row = []
             for i in range(camera.W):
                 x = (2 * (i + 0.5) / camera.W - 1) * focal
                 y = (1 - 2 * (j + 0.5) / camera.H) * focal / aspectRatio
@@ -70,18 +81,19 @@ class Render:
                 ray_dir.normalize_()
                 ray_orig = camera.position
                 c = self.integrate(ray_orig, ray_dir, obj, bg)
-                result.append(str(int(clip(c.x, 0, 1)*255)))
-                result.append(str(int(clip(c.y, 0, 1)*255)))
-                result.append(str(int(clip(c.z, 0, 1)*255)))
-
+                row.append(str(int(clip(c.x, 0, 1)*255)))
+                row.append(str(int(clip(c.y, 0, 1)*255)))
+                row.append(str(int(clip(c.z, 0, 1)*255)))
+            result.append(row)
+        
         with open('vr.ppm', 'w') as f:
             f.write("P3\n")
             f.write(f"{camera.W} {camera.H}\n")
             f.write("255\n")
-            f.write(' '.join(result))
-
-
-
+            for row in result:
+                f.write(' '.join(row))
+                f.write('\n')
+        
 from objects import Sphere
 #Light direction
 light_dir = Vec3(0., -1., 0.)
@@ -92,7 +104,7 @@ p = identity_phase_fn()
 p = uniform_phase_fn()
 p = greenstein_phase_fn(g=.0)
 # Object
-obj = Sphere(absorption=.3, scattering=0.5, phase=p)
+obj = Sphere(absorption=.5, scattering=0.5, phase=p)
 # Camera
 cam = Camera(980, 512, fov=60, position=Vec3(0, 0, 4))
 # Renderer
